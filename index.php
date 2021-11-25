8f343f38f1a0bdde8474a32f001f988583290c9f
Keith Asare <78114242+BigDOA@users.noreply.github.com>
2021-11-25 07:31:01 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+include "includes/config.php";
+session_start();
+
+if (isset($_SESSION["user_email"])) {
+    header("Location: todos.php");
+    die();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <?php getHead(); ?>
+</head>
+<body>
+<div class="container col-xl-10 col-xxl-8 px-4 py-5">
+        <div class="row align-items-center g-lg-5 py-5">
+            <div class="col-lg-7 text-center text-lg-start">
+                <h1 class="display-4 fw-bold lh-1 mb-3">Login to continue creating your list</h1>
+            </div>
+            <div class="col-md-10 mx-auto col-lg-5">
+                <form action="login.php" method="POST" class="p-4 p-md-5 border rounded-3 bg-light">
+                    <div class="form-floating mb-3">
+                        <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Email address</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Password</label>
+                    </div>
+                    <button name="submit" class="w-100 btn btn-lg btn-primary" type="submit">Continue</button>
+                    <hr class="my-4">
+                    <small class="text-muted">By clicking Continue, you agree to the terms of use.</small>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
