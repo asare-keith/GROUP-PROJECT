652246cd3794252cf38597fc597e8f096bc10523
JacobTwum-K <77839319+JacobTwum-K@users.noreply.github.com>
2021-11-26 21:14:33 +0000

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (isset($_SESSION["user_email"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To-do List</title>
+    <title>Homepage</title>
     <link rel="stylesheet" href="style.css">
     <?php getHead(); ?>
 </head>
